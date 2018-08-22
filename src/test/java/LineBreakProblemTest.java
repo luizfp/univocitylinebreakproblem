a83a8a666cb8fa3159be347ffd3cb1d79035e185
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-08-22 10:35:01 -0300

    Initial commit

---
+++ src/test/java/LineBreakProblemTest.java
@@ -0,0 +1,30 @@
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created on 22/08/2018
+ *
+ * @author Luiz Felipe (https://github.com/luizfp)
+ */
+public class LineBreakProblemTest {
+    private static final String TARGET_PATH = System.getProperty("user.dir");
+    private static final String FILE_NAME = "file_test.csv";
+
+    @Test
+    public void testLineBreak() {
+        final File file = new File(TARGET_PATH, FILE_NAME);
+        final CsvParserSettings settings = new CsvParserSettings();
+        settings.setDelimiterDetectionEnabled(true);
+        settings.setHeaderExtractionEnabled(true);
+        final CsvParser parser = new CsvParser(settings);
+        final List<String[]> rows = parser.parseAll(file);
+        assertEquals(1, rows.size());
+        assertEquals(1, rows.get(0).length);
+    }
+}
